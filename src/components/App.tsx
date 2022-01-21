739ad1c64a2029e6005fe8c352f2915b351f6557
y.hoshi <ysk.hoshi.1118@gmail.com>
2022-01-21 12:42:28 +0900

    add all operation log

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { EventForm } from './EventForm';
 import { Events } from './Events';
+import { OperationLogs } from './OperationLogs';
 import reducer from '../reducers';
 import { AppStateType, SetAppContextProvider } from '../contexts/AppContext';
 
@@ -18,6 +19,7 @@ const App: React.VFC = () => {
       <div className="container-fluid">
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </SetAppContextProvider>
   );
