b091a3d56ae7b8e3c6a2004f8b0425845cbc5c8f
y.hoshi <ysk.hoshi.1118@gmail.com>
2022-01-21 13:05:01 +0900

    seve state with localStorage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { EventForm } from './EventForm';
@@ -7,12 +7,23 @@ import { OperationLogs } from './OperationLogs';
 import reducer from '../reducers';
 import { AppStateType, SetAppContextProvider } from '../contexts/AppContext';
 
+const APP_KEY = 'appWithRedux';
+
 const App: React.VFC = () => {
-  const initialState: AppStateType = {
-    events: [],
-    operationLogs: [],
-  };
+  const appState = localStorage.getItem(APP_KEY);
+
+  const initialState: AppStateType = appState
+    ? JSON.parse(appState)
+    : {
+        events: [],
+        operationLogs: [],
+      };
+
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  }, [state]);
 
   return (
     <SetAppContextProvider value={{ state, dispatch }}>
